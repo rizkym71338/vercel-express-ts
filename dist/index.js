ef5ea36752fa7a60a67097932e141b90daf5171b
Rizky Maulana <rizkym71338@gmail.com>
2024-06-26 13:31:49 +0700

    test: ping route

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,9 @@ const express_1 = __importDefault(require("express"));
 const env_1 = require("./libs/env");
 const app = (0, express_1.default)();
 app.get('/', (_, response) => {
-    response.status(200).send('Welcome to vercel-express-ts!');
+    response.status(200).json({ message: 'Welcome to vercel-express-ts' });
+});
+app.get('/ping', (_, response) => {
+    response.status(200).json({ message: 'pong!' });
 });
 app.listen(env_1.PORT, () => console.log('Server running at PORT: ', env_1.PORT));
